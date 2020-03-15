735b448d1354fe2edaf3d430a6848b63304bcba3
YoungCapta1n <huzhuhuan1991@gmail.com>
2020-03-15 11:26:09 +0800

    osqp example

---
+++ common/math/solvers/osqp/test.c
@@ -0,0 +1,100 @@
+#include "osqp.h"
+
+int main(int argc, char **argv) {
+  // Load problem data
+  c_float P_x[4] = {
+      2.0,
+      0.2,
+      -1.0,
+      2.0,
+  };
+  c_int P_nnz = 4;
+  c_int P_i[4] = {
+      0,
+      1,
+      0,
+      2,
+  };
+  c_int P_p[4] = {
+      0,
+      1,
+      2,
+      4,
+  };
+  c_float q[3] = {
+      0.0,
+      -1.0,
+      0.0,
+  };
+  c_float A_x[6] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+  c_int A_nnz = 6;
+  c_int A_i[6] = {
+      0, 1, 0, 2, 0, 3,
+  };
+  c_int A_p[4] = {
+      0,
+      2,
+      4,
+      6,
+  };
+  c_float l[4] = {1.0, 0.0, 0.0, 0.0};
+  c_float u[4] = {
+      OSQP_INFTY,
+      OSQP_INFTY,
+      OSQP_INFTY,
+      OSQP_INFTY,
+  };
+  c_int n = 3;
+  c_int m = 4;
+
+  // Exitflag
+  c_int exitflag = 0;
+
+  // Workspace structures
+  OSQPWorkspace *work;
+  OSQPSettings *settings = (OSQPSettings *)c_malloc(sizeof(OSQPSettings));
+  OSQPData *data = (OSQPData *)c_malloc(sizeof(OSQPData));
+
+  // Populate data
+  if (data) {
+    data->n = n;
+    data->m = m;
+    data->P = csc_matrix(data->n, data->n, P_nnz, P_x, P_i, P_p);
+    data->q = q;
+    data->A = csc_matrix(data->m, data->n, A_nnz, A_x, A_i, A_p);
+    data->l = l;
+    data->u = u;
+  }
+
+  // Define solver settings as default
+  if (settings) {
+    osqp_set_default_settings(settings);
+    settings->alpha = 1.0;  // Change alpha parameter
+  }
+
+  // Setup workspace
+  exitflag = osqp_setup(&work, data, settings);
+
+  // Solve Problem
+  osqp_solve(work);
+
+  // solution
+  OSQPSolution *sol_data = work->solution;
+
+  // print solution
+  printf("Optimal primal solution: \n");
+  for (int i = 0; i != 3; ++i) printf("x[%d]: %f\n", i, sol_data->x[i]);
+
+  // Clean workspace
+  osqp_cleanup(work);
+
+  // Cleanup
+  if (data) {
+    if (data->P) c_free(data->P);
+    if (data->A) c_free(data->A);
+    c_free(data);
+  }
+  if (settings) c_free(settings);
+
+  return exitflag;
+};
