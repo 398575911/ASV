5d8ab1de741a7807fef2f053fc6e090b0576765a
YoungCapta1n <huzhuhuan1991@gmail.com>
2020-04-07 22:24:07 +0800

    kd tree

---
+++ modules/planner/path_planning/openspace/include/PathSmoother.h
@@ -0,0 +1,24 @@
+/*
+***********************************************************************
+* PathSmoothing.h:
+* improve the smoothness of path, using conjugate-gradeient descent
+* This header file can be read by C++ compilers
+*
+* by Hu.ZH(CrossOcean.ai)
+***********************************************************************
+*/
+
+#ifndef _PATHSMOOTHING_H_
+#define _PATHSMOOTHING_H_
+
+#include "openspacedata.h"
+
+namespace ASV::planning {
+
+class PathSmoothing {
+ public:
+ private:
+};  // end class PathSmoothing
+}  // namespace ASV::planning
+
+#endif /* _PATHSMOOTHING_H_ */
