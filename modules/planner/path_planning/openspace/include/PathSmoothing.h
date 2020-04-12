f20e0258c04b93252fea95fa0cedf836c69b14d2
YoungCapta1n <huzhuhuan1991@gmail.com>
2020-04-12 15:39:09 +0800

    smoother

--- modules/planner/path_planning/openspace/include/PathSmoothing.h
+++ modules/planner/path_planning/openspace/include/PathSmoothing.h
@@ -25,6 +25,7 @@ class PathSmoothing {
   explicit PathSmoothing(const SmootherConfig &smootherconfig)
       : dmax_(smootherconfig.d_max),
         kappa_max_(0),
+        fine_discrete_(0.05),
         x_resolution_(0.01),
         y_resolution_(0.01),
         theta_resolution_(0.01),
@@ -39,15 +40,49 @@ class PathSmoothing {
     coarse_path_ = FindForwardReverseSwitch(path);
     assert(coarse_path_.size() > 3);
     return *this;
+  }  // SetupCoarsePath
+
+  PathSmoothing &PerformSmoothing(
+      const CollisionChecking_Astar &collision_checker) {
+    for (int i = 0; i != 1; ++i) {
+      std::vector<std::vector<vec2d>> _nearest_obstacles =
+          GenerateNearestNeighbors(collision_checker, coarse_path_, dmax_);
+
+      // compute the gradient of cost function
+      std::vector<vec2d> _gradient = GenerateGradient(
+          _nearest_obstacles, coarse_path_, dmax_, omega_o_, omega_s_);
+
+      // backtracking line search
+      double gamma = 0.01;  // 0 < gamma
+
+      double _cost = GenerateCost(_nearest_obstacles, coarse_path_, dmax_,
+                                  omega_o_, omega_s_);
+
+      std::cout << "cost: " << _cost << std::endl;
+      // update the coarse path
+      UpdateTrajectory(coarse_path_, _gradient, gamma);
+      _cost = GenerateCost(_nearest_obstacles, coarse_path_, dmax_, omega_o_,
+                           omega_s_);
+      std::cout << "new cost: " << _cost << std::endl;
+    }
+    return *this;
+  }  // PerformSmoothing
+
+  void InterpolateTrajectory() {
+    coarse_path_;
+    // super sample
+
+    // hold the orignal vertex and minimize the curvature
   }
 
-  vec4t PerformSmoothing() {}
-
   auto coarse_path() const noexcept { return coarse_path_; }
+  auto smooth_path() const noexcept { return smooth_path_; }
+  auto fine_path() const noexcept { return fine_path_; }
 
  private:
   const double dmax_;              // m
   const double kappa_max_;         // 1/m
+  const double fine_discrete_;     // m
   const double x_resolution_;      // m
   const double y_resolution_;      // m
   const double theta_resolution_;  // rad
@@ -59,7 +94,137 @@ class PathSmoothing {
   mutable vec3t coarse_path_;
 
   // fine path
-  mutable std::vector<vec2d> fine_path_;
+  mutable vec3t smooth_path_;
+  mutable std::vector<std::array<double, 3>> fine_path_;
+
+  std::vector<std::vector<vec2d>> GenerateNearestNeighbors(
+      const CollisionChecking_Astar &collision_checker, const vec3t &path,
+      const double dmax) const {
+    std::size_t size_coarse_path = path.size();
+    std::vector<std::vector<vec2d>> _nearest_obstacles(size_coarse_path,
+                                                       {{0, 0}});
+    // gradient at start/end and fixed node are all zero
+    for (std::size_t index = 1; index != (size_coarse_path - 1); ++index) {
+      if (!std::get<1>(path[index])) {  // not fixed node
+        // find the nearest neighbors around the current node
+        auto i_nearest_obstacles = collision_checker.FindNearestNeighbors(
+            std::get<0>(path[index]).x(), std::get<0>(path[index]).y(), dmax);
+        _nearest_obstacles[index] = i_nearest_obstacles;
+      }
+    }
+    return _nearest_obstacles;
+  }  // GenerateNearestNeighbors
+
+  std::vector<vec2d> GenerateGradient(
+      const std::vector<std::vector<vec2d>> &nearest_obstacles,
+      const vec3t &path, const double dmax, const double omega_o,
+      const double omega_s) const {
+    std::size_t size_coarse_path = path.size();
+
+    // compute the gradient of cost function
+    std::vector<vec2d> _gradient(size_coarse_path, {0, 0});
+    std::vector<vec2d> _Xim1_Xi_Xip1(size_coarse_path, {0, 0});
+
+    for (std::size_t index = 1; index != (size_coarse_path - 1); ++index) {
+      _Xim1_Xi_Xip1[index] = std::get<0>(path[index - 1]) +
+                             std::get<0>(path[index + 1]) -
+                             std::get<0>(path[index]) * 2;
+    }
+
+    // gradient at start/end and fixed node are all zero
+    for (std::size_t index = 1; index != (size_coarse_path - 1); ++index) {
+      if (!std::get<1>(path[index])) {  // not fixed node
+
+        auto i_coarse_path = std::get<0>(path[index]);
+
+        // gradient obstacle
+        vec2d i_gradient_obstacle(0, 0);
+        for (const auto &nearest_obstacle : nearest_obstacles[index]) {
+          // add the obstacle potential in the nearst neighbors
+          auto x2o = i_coarse_path - nearest_obstacle;
+          // assume the x2o is not very small
+          double k = 1.0 - dmax / x2o.Length();
+          i_gradient_obstacle += (x2o * k);
+        }
+
+        // gradient smoothing
+        vec2d i_gradient_smooth = _Xim1_Xi_Xip1[index - 1] +
+                                  _Xim1_Xi_Xip1[index + 1] -
+                                  _Xim1_Xi_Xip1[index] * 2;
+
+        // add all gradients
+        _gradient[index] =
+            i_gradient_smooth * omega_s + i_gradient_obstacle * omega_o;
+
+      }  // end if
+    }    // end for loop
+    return _gradient;
+  }  // GenerateGradient
+
+  // compute the cost value given the current trajectory
+  double GenerateCost(const std::vector<std::vector<vec2d>> &nearest_obstacles,
+                      const vec3t &path, const double dmax,
+                      const double omega_o, const double omega_s) const {
+    double _obstacle_cost = 0.0;
+    double _smooth_cost = 0.0;
+
+    std::size_t size_coarse_path = path.size();
+
+    // gradient at start/end and fixed node are all zero
+    for (std::size_t index = 1; index != (size_coarse_path - 1); ++index) {
+      if (!std::get<1>(path[index])) {  // not fixed node
+
+        auto i_coarse_path = std::get<0>(path[index]);
+
+        // cost obstacle
+        double i_obstacle = 0;
+        for (const auto &nearest_obstacle : nearest_obstacles[index]) {
+          // add the obstacle potential in the nearst neighbors
+          auto x2o = i_coarse_path - nearest_obstacle;
+          i_obstacle += std::pow(dmax - x2o.Length(), 2);
+        }
+        _obstacle_cost += i_obstacle;
+
+        // cost smoothing
+        auto i_Xim1_Xi_Xip1 = std::get<0>(path[index - 1]) +
+                              std::get<0>(path[index + 1]) - i_coarse_path * 2;
+        _smooth_cost += i_Xim1_Xi_Xip1.LengthSquare();
+
+      }  // end if
+    }    // end for loop
+    return _obstacle_cost * omega_o + _smooth_cost * omega_s;
+
+  }  // GenerateCost
+
+  // update the coarse path
+  void UpdateTrajectory(vec3t &path, const std::vector<vec2d> &gradient,
+                        const double gamma) const {
+    for (std::size_t index = 0; index != path.size(); ++index)
+      std::get<0>(path[index]) -= (gradient[index] * gamma);
+  }  // UpdateTrajectory
+
+  // super sample the smooth path, with smooth vertex fixed
+  vec3t SuperSample(const vec3t &smoothpath) const {
+    vec3t fine_path;
+
+    std::size_t size_smooth_path = smoothpath.size();
+    for (std::size_t index = 0; index != (size_smooth_path - 1); ++index) {
+      auto i_smooth_path = std::get<0>(smoothpath[index]);
+      // generate the fixed vertex
+      fine_path.push_back({i_smooth_path, true});
+      // linear intepolation
+      auto diff_vec2d = std::get<0>(smoothpath[index + 1]) - i_smooth_path;
+      double diff_length = diff_vec2d.Length();
+      for (double j = fine_discrete_; j < diff_length; j += fine_discrete_) {
+        fine_path.push_back(
+            {i_smooth_path + diff_vec2d * (j / diff_length), false});
+      }
+    }
+    // don't forget the last one
+    fine_path.push_back({std::get<0>(smoothpath.back()), true});
+    return fine_path;
+
+  }  // SuperSample
 
   // compute the gradient of obstacle term
   std::vector<vec2d> GenerateObstacleTerm(
@@ -185,14 +350,11 @@ class PathSmoothing {
 
     for (const auto &current_state : coarse_trajectory) {
       auto processed_coarse_last = processed_coarse_trajectory.back();
-      if ((std::abs(std::get<0>(processed_coarse_last) -
-                    std::get<0>(current_state)) <= x_resolution_) &&
-          (std::abs(std::get<1>(processed_coarse_last) -
-                    std::get<1>(current_state)) <= y_resolution_) &&
-          (std::abs(ASV::common::math::Normalizeheadingangle(
-               std::get<2>(processed_coarse_last) -
-               std::get<2>(current_state))) <=
-           theta_resolution_)) {  // same state
+      if (IsSameNode(std::get<0>(processed_coarse_last),
+                     std::get<1>(processed_coarse_last),
+                     std::get<2>(processed_coarse_last),
+                     std::get<0>(current_state), std::get<1>(current_state),
+                     std::get<2>(current_state))) {  // same state
         if (std::get<3>(processed_coarse_last) == std::get<3>(current_state)) {
           std::get<1>(processed_coarse_vertex.back()) = false;
         } else
@@ -207,6 +369,16 @@ class PathSmoothing {
     }
     return processed_coarse_vertex;
   }  // FindForwardReverseSwitch
+
+  // check if two node is closed
+  bool IsSameNode(const double lhs_x, const double lhs_y,
+                  const double lhs_theta, const double rhs_x,
+                  const double rhs_y, const double rhs_theta) const {
+    return ((std::abs(lhs_x - rhs_x) <= x_resolution_) &&
+            (std::abs(lhs_y - rhs_y) <= y_resolution_) &&
+            (std::abs(ASV::common::math::Normalizeheadingangle(
+                 lhs_theta - rhs_theta)) <= theta_resolution_));
+  }  // IsSameNode
 
 };  // end class PathSmoothing
 }  // namespace ASV::planning
--- modules/planner/path_planning/openspace/test/Smoothing_test.cc
+++ modules/planner/path_planning/openspace/test/Smoothing_test.cc
@@ -8,13 +8,32 @@
 */
 
 #include "../include/PathSmoothing.h"
+#include "DataFactory.hpp"
+
+using namespace ASV::planning;
 
 int main() {
-  ASV::planning::SmootherConfig smoothconfig{
-      1,
+  int test_scenario = 4;
+
+  // obstacles
+  std::vector<Obstacle_Vertex_Config> Obstacles_Vertex;
+  std::vector<Obstacle_LineSegment_Config> Obstacles_LS;
+  std::vector<Obstacle_Box2d_Config> Obstacles_Box;
+  std::array<float, 3> start_point;
+  std::array<float, 3> end_point;
+  generate_obstacle_map(Obstacles_Vertex, Obstacles_LS, Obstacles_Box,
+                        start_point, end_point, test_scenario);
+
+  CollisionChecking_Astar collision_checker_(_collisiondata);
+  collision_checker_.set_all_obstacls(Obstacles_Vertex, Obstacles_LS,
+                                      Obstacles_Box);
+
+  // Path Smoothing
+  SmootherConfig smoothconfig{
+      10,  // d_max
   };
 
-  ASV::planning::PathSmoothing pathsmoother(smoothconfig);
+  PathSmoothing pathsmoother(smoothconfig);
 
   // std::vector<std::tuple<double, double, double, bool>> coarse_path = {
   //     {30, 7, 0, true},
@@ -54,32 +73,73 @@ int main() {
   //     {21.5, 3.5, -1.64581, true},
   // };
 
+  // std::vector<std::tuple<double, double, double, bool>> coarse_path = {
+  //     {30, 7, 0, true},
+  //     {29, 7, 8.7e-8, true},
+  //     {29, 7, 8.7e-8, false},
+  //     {28, 7, 8.7e-8, false},
+  //     {27, 7, 8.7e-8, false},
+  //     {26, 7, 8.7e-8, false},
+  //     {25, 7, 8.7e-8, false},
+  //     {24.5019, 6.96257, 0.15, false},
+  //     {24.0149, 6.85112, 0.3, false},
+  //     {23.5501, 6.66816, 0.45, false},
+  //     {23.1179, 6.41779, 0.60, false},
+  //     {23.7279, 6.10563, 0.75, false},
+  //     {22.3889, 5.7387, 0.90, false},
+  //     {22.1086, 5.32524, 1.05, false},
+  //     {21.8932, 4.87453, 1.2, false},
+  //     {21.7476, 4.39669, 1.35, false},
+  //     {21.675, 3.90246, 1.49402, false},
+  //     {21.6366, 3.40393, 1.49402, false},
+  //     {21.5983, 2.90541, 1.49402, false},
+  //     {21.5599, 2.40688, 1.49402, false},
+  //     {21.5216, 1.90835, 1.49402, false},
+  //     {21.5, 1.5, 1.5708, false},
+
+  // };
+
   std::vector<std::tuple<double, double, double, bool>> coarse_path = {
-      {30, 7, 0, true},
-      {29, 7, 8.7e-8, true},
-      {29, 7, 8.7e-8, false},
-      {28, 7, 8.7e-8, false},
-      {27, 7, 8.7e-8, false},
-      {26, 7, 8.7e-8, false},
-      {25, 7, 8.7e-8, false},
-      {24.5019, 6.96257, 0.15, false},
-      {24.0149, 6.85112, 0.3, false},
-      {23.5501, 6.66816, 0.45, false},
-      {23.1179, 6.41779, 0.60, false},
-      {23.7279, 6.10563, 0.75, false},
-      {22.3889, 5.7387, 0.90, false},
-      {22.1086, 5.32524, 1.05, false},
-      {21.8932, 4.87453, 1.2, false},
-      {21.7476, 4.39669, 1.35, false},
-      {21.675, 3.90246, 1.49402, false},
-      {21.6366, 3.40393, 1.49402, false},
-      {21.5983, 2.90541, 1.49402, false},
-      {21.5599, 2.40688, 1.49402, false},
-      {21.5216, 1.90835, 1.49402, false},
-      {21.5, 1.5, 1.5708, false},
+      {4, 6, 0.942478, true},
+      {4.69945, 6.70943, 0.642478, true},
+      {5.57732, 7.18047, 0.342378, true},
+      {6.55517, 7.37104, 0.042478, true},
+      {7.54567, 7.26413, -0.257522, true},
+      {8.46024, 6.86928, -0.557522, true},
+      {9.21747, 6.22176, -0.857522, true},
+      {9.9746, 5.57424, -0.557522, true},
+      {10.8893, 5.17939, -0.257522, true},
+      {11.7902, 4.79047, -0.557522, true},
+      {12.7049, 4.39562, -0.257522, true},
+      {13.6954, 4.28871, 0.0424779, true},
+      {14.6732, 4.47928, 0.342478, true},
+      {14.6732, 4.47928, 0.342478, false},
+      {14.2166, 4.27675, 0.492478, false},
+      {13.7954, 4.00824, 0.642478, false},
+      {13.419, 3.67981, 0.792478, false},
+      {13.0959, 3.29882, 0.942478, false},
+      {12.8278, 2.87705, 1.00435, false},
+      {12.5289, 2.47681, 0.854345, false},
+      {12.1735, 2.12574, 0.704345, false},
+      {11.7697, 1.8317, 0.554345, false},
+      {11.3265, 1.60132, 0.404345, false},
+      {10.8538, 1.43976, 0.254345, false},
+      {10.7575, 1.41626, 0.224623, false},
+      {10.7575, 1.41626, 0.224623, true},
+      {11.2515, 1.49072, 0.074623, true},
+      {11.5, 1.5, 0.0, true},
 
   };
   auto p_coarse = pathsmoother.SetupCoarsePath(coarse_path).coarse_path();
+  pathsmoother.PerformSmoothing(collision_checker_);
+
+  // plotting
+  Gnuplot gp;
+  gp << "set terminal x11 size 1100, 1100 0\n";
+  gp << "set title 'Path Smoothing'\n";
+  gp << "set xrange [0:50]\n";
+  gp << "set yrange [0:50]\n";
+  gp << "set size ratio -1\n";
 
   for (const auto &value : p_coarse) {
     std::cout << std::get<0>(value).x() << ", " << std::get<0>(value).y()
